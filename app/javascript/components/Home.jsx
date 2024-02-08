4e260a1aceb28f2335b1eb1410957c7f1ac71a9f
Todd Chase <toddchase38@gmail.com>
2024-02-08 15:27:35 -0500

    Handle thru model Recipe

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+    <div className="jumbotron jumbotron-fluid bg-transparent">
+      <div className="container secondary-color">
+        <h1 className="display-4">Todd Place</h1>
+        <p className="lead">
+          Welcome to my homepage.
+        </p>
+        <hr className="my-4" />
+        <Link
+          to="/recipes"
+          className="btn btn-lg custom-button"
+          role="button"
+        >
+          View Projects
+        </Link>
+      </div>
+    </div>
+  </div>
+);
