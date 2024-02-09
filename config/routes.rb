5d9a809ceefb6a8c35c4acf248242b66ad019a6a
Todd Chase <toddchase38@gmail.com>
2024-02-09 14:50:34 -0500

    WIP: For some reason the app does not render the Home component, instaed it just goes right to the recipes url

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
+  root 'homepage#index'
+
   namespace :api do
     namespace :v1 do
       get 'recipes/index'
       post 'recipes/create'
       get '/show/:id', to:'recipes#show'
-      get 'recipes/destroy'
+      delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
-  root 'homepage#index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/*path' => 'homepage#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
