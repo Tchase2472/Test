ecdbe5dd8c24e45940ec608d1b3b875eb772cfd1
Todd Chase <toddchase38@gmail.com>
2024-02-13 15:20:08 -0500

    Fixed the recipe component to render properly

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -4,20 +4,21 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 const Recipe = () => {
     const params = useParams();
     const navigate = useNavigate();
-    const [recipe, setRecipe] = useState({ingredients: ""});
+    const [recipe, setRecipe] = useState({ingredients: "" });
 
-    useEffect(() => {
-        const url = `api/v1/show/${params.id}`;
-        fetch(url)
-           .then ((response)=> {
-            if (response.ok){
-                return response.json();
-            }
-            throw new Error("Network response was not OK");
-           })
-           .then((response) => setRecipe(response))
-           .catch(() => navigate("/recipes"));
-    }, [params.id]);
+    
+  useEffect(() => {
+    const url = `/api/v1/show/${params.id}`;
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => setRecipe(response))
+      .catch(() => navigate("/recipes"));
+  }, [params.id]);
 
     const addHtmlEntities = (str) => {
         return String(str).replace(/&lt;/g, "<").replace(/&gt;/g, ">");
--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -7,9 +7,9 @@ import Recipe from "../components/Recipe";
 export default (
   <Router>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />}  />
       <Route path="/recipes" element={<Recipes />} />
-      <Route path="/recipe/:id" element={<Recipe />} />
+      <Route path="/recipe/:id" element={<Recipe /> } />
     </Routes>
   </Router>
 );
